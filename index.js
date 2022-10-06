f0ff14b865d3e2e4b5edb713d7aff6b37aa76ae4
Moringa School <marymeoli77@gmail.com>
2022-10-06 13:10:08 +0300

    compelet assignment

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// batteryBatches.reduce(batteries, 0)
+
+
+//function batteries(accumulator, element){
+//   let totalBatteries = accumulator + element
+//  return totalBatteries
+//}
+
+ let totalBatteries = batteryBatches.reduce(function (accumulator, element){
+     return accumulator + element},0)
+  //  code your solution here
